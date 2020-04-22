1682e508d1d46242546e12543ee4e880330f46f4
wayman <hegaomin@ejoyst.com>
2020-04-22 09:35:14 +0800

    update 修改路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, Route } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 
 import Nothing from '../view/nothing/index'
 import Contrast from '../view/contrast/index'
@@ -9,9 +9,11 @@ export default class Routes extends React.Component {
     render() {
         return (
             <HashRouter>
-                <Route exact path='/contrast' component={Contrast} />
-                <Route exact path='/report' component={Report} />
-                <Route exact path='*' component={Nothing} />
+                <Switch>
+                    <Route path='/contrast' component={Contrast} />
+                    <Route path='/report' component={Report} />
+                    <Route component={Nothing} />
+                </Switch>
             </HashRouter>
         )
     }
